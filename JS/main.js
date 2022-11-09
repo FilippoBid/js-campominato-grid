d020702c252ed34d2fab9ee98f24ea3d75561521
FilippoBid <filippoperrone1997@gmail.com>
2022-11-09 17:24:17 +0100

    esercizio completato lavoro sui bonus

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,4 @@
 const startBtn = document.getElementById("startbtn");
-const containerEl = document.querySelector(".container");
 
 let NSquare = 100;
 let NRow = 10;
@@ -9,15 +8,21 @@ let NCol = 10;
 startBtn.addEventListener("click", function () {
     
     for (i = 0; i < NSquare; i++) {
-
+        
+        const containerEl = document.querySelector(".container");
         const square = document.createElement("div");
         square.classList.add("squarestyle");
         square.classList.add("text-center");
         square.classList.add("vertical-align");
+        square.addEventListener("click",function(){
+            this.classList.toggle("bg-danger")
+            console.log(square.innerHTML);
+        })
        
         square.style.width = `calc(100% / ${NCol})`
         square.innerHTML = `${i + 1}`
         containerEl.append(square);
+        
 
 
     }
