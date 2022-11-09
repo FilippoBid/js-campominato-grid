53c7a119d4715ca42fbd805bc507f5807c30e8e4
FilippoBid <filippoperrone1997@gmail.com>
2022-11-09 20:39:15 +0100

    fix bonus part dannato bottone !

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,4 @@
-const startBtn = document.getElementById("startbtn");
+/* const startBtn = document.getElementById("startbtn");
 
 let NSquare = 100;
 let NRow = 10;
@@ -31,4 +31,83 @@ startBtn.addEventListener("click", function () {
 
 
 
-})
+}) //esercizio senza bonus*/
+
+
+
+
+
+
+
+
+/* let NSquare = 100;
+let NRow = 10;
+let NCol = 10; */
+const startBtn = document.getElementById("startbtn");
+const selectDEl = document.querySelector("select");
+
+const containerEl = document.querySelector(".container");
+/**
+ * 
+ * 
+ * @param {number} selCol 
+ * @param {number} containerSquare 
+ * @return {Element}  
+ */
+
+function creaGrid (selCol,containerSquare){
+    containerEl.innerHTML=""
+    let NSquare;
+    let NCol;
+    
+    
+    if(selCol == "1"){
+        NSquare = 100;
+        NCol = 10;    
+    }else if(selCol === "2"){
+        NSquare = 81;
+        NCol = 9;
+    }else if(selCol === "3"){
+        NSquare = 49;
+        NCol = 7;
+    }
+    
+    
+    
+    
+    
+    for (i = 0; i < NSquare; i++){
+        let square = document.createElement("div");
+        square.classList.add("squarestyle");
+        square.classList.add("text-center");
+        square.classList.add("vertical-align");
+        square.addEventListener("click",function(){
+            this.classList.toggle("bg-danger")
+            console.log(square.innerHTML);
+        })
+        
+        square.style.width = `calc(100% / ${NCol})`
+        square.innerHTML = `${i + 1}`
+        containerSquare.append(square);
+    }
+    
+    
+    
+    
+    
+    
+}
+
+
+
+
+startBtn.addEventListener("click", function(){
+    const selectD = selectDEl.value;
+    creaGrid (selectD,containerEl);
+    console.log(selectDEl);
+    console.log(selectD);
+});
+
+
+
+
